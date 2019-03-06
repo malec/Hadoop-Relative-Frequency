bba013828b4593dffa0d238353563a875a75f41c
malec <alec@ahlbrandt.net>
2019-03-05 19:00:19 -0600

    All working

--- part1/RelativeFreq1.java
+++ part1/RelativeFreq1.java
@@ -1,87 +1,100 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class RelativeFreq1 {
-	public static class PairsOccurrenceMapper extends Mapper<LongWritable, Text, WordPair, IntWritable> {
-		private WordPair wordPair = new WordPair();
-		private IntWritable ONE = new IntWritable(1);
+	public static class Map extends Mapper<Object, Text, Text, IntWritable> {
+		private static final IntWritable one = new IntWritable(1);
 
-		@Override
-		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			int neighbors = context.getConfiguration().getInt("neighbors", 2);
-			String[] tokens = value.toString().split("\\s+");
-			if (tokens.length > 1) {
-				for (int i = 0; i < tokens.length; i++) {
-					wordPair.setWord(tokens[i]);
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			String[] words = value.toString().split(" ");
+
+			for (String word : words) {
+				if (word.matches("^\\w+$")) {
 					int count = 0;
-					int start = (i - neighbors < 0) ? 0 : i - neighbors;
-					int end = (i + neighbors > tokens.length - 1) ? tokens.length - 1 : i + neighbors;
-					for (int j = start; j <= end; j++) {
-						if (j == i) continue;
-							wordPair.setNeighbor(tokens[j]);
-							context.write(new WordPair(wordPair.getWord(), new Text("*")), ONE);
+					for (String term : words) {
+						if (term.matches("^\\w+$") && !term.equals(word)) {
+							context.write(new Text(word + "," + term), one);
 							count++;
+						}
 					}
-					context.write(new WordPair(tokens[i], "*"), new IntWritable(count));
+					context.write(new Text(word + ",*"), new IntWritable(count));
+				} else {
+					System.out.println("word is: \"" + word + "\"");
 				}
 			}
 		}
 	}
 
-	private static class RFCombiner extends Reducer<WordPair, IntWritable, WordPair, IntWritable> {
-		public void reduce(WordPair key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-				int count = 0;
-				for (IntWritable value : values) {
-						count += Integer.parseInt(value.toString());
-				}
-				context.write(key, new IntWritable(count));
-		}
-}
-
-	public static class PairsReducer extends Reducer<WordPair, IntWritable, WordPair, IntWritable> {
-		private IntWritable totalCount = new IntWritable();
-
+	public static class RFPartitioner extends Partitioner<Text, IntWritable> {
 		@Override
-		protected void reduce(WordPair key, Iterable<IntWritable> values, Context context)
-				throws IOException, InterruptedException {
+		public int getPartition(Text key, IntWritable value, int numReduceTasks) {
+			char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+					't', 'u', 'v', 'w', 'x', 'y', 'z' };
+			return (java.util.Arrays.binarySearch(alphabet, Character.toLowerCase(key.toString().split(",")[0].charAt(0)))
+					* numReduceTasks / alphabet.length) % numReduceTasks;
+		}
+	}
+
+	private static class Combine extends Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 			int count = 0;
+			for (Text value : values) {
+				count += Integer.parseInt(value.toString());
+			}
+			context.write(key, new IntWritable(count));
+		}
+	}
+
+	public static class Reduce extends Reducer<Text, IntWritable, Text, Text> {
+		double totalCount = 0;
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			String keyStr = key.toString();
+			int count = 0;
+
 			for (IntWritable value : values) {
 				count += value.get();
 			}
-			totalCount.set(count);
-			context.write(key, totalCount);
+
+			if (keyStr.matches(".*\\*")) {
+				totalCount = count;
+			} else {
+				String[] pair = keyStr.split(",");
+				context.write(new Text(pair[0] + ", " + pair[1]), new Text(String.valueOf(count / totalCount)));
+			}
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-		if (otherArgs.length != 2) {
-			System.err.println("Usage: wordcount <in> <out>");
-			System.exit(2);
-		}
-		Job job = new Job(conf, "word occurence");
+		Job job = new Job(new Configuration());
 		job.setJarByClass(RelativeFreq1.class);
-		
-		job.setMapperClass(PairsOccurrenceMapper.class);
-		job.setReducerClass(PairsReducer.class);
-		job.setCombinerClass(RFCombiner.class);
+		job.setNumReduceTasks(3);
 
-		job.setOutputKeyClass(WordPair.class);
-		job.setOutputValueClass(IntWritable.class);
-		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		job.setMapperClass(Map.class);
+		job.setPartitionerClass(RFPartitioner.class);
+		job.setCombinerClass(Combine.class);
+		job.setReducerClass(Reduce.class);
+
+		FileInputFormat.addInputPath(job, new Path("input"));
+		FileOutputFormat.setOutputPath(job, new Path("output"));
+
+		job.waitForCompletion(true);
 	}
 }
